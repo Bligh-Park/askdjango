c33c4577bbc043e5d9a5729e021fd938b3d0246d
Bligh Park <sy131.park@gmail.com>
2017-04-03 23:29:44 +0900

    blog.models.Post 모델 - tag, 경도/위도 설정 추가

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,18 @@
 from django.db import models
+import re
+from django.forms import ValidationError
+
+def lnglat_validator(value):
+    if not re.match(r'^([+-]?\d+\.?\d*),([+-]?\d+\.?\d*)$', value):
+        raise ValidationError('Invalid LngLat Type')
 
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    tags = models.CharField(max_length=100, blank=True)
+    lnglat = models.CharField(max_length=50, blank = True,
+                              validators=[lnglat_validator],
+                              help_text='경도,위도 포맷으로 입력')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
