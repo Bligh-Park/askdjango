b1f60e967454ba39143485afd7b153c5ba375453
Bligh Park <sy131.park@gmail.com>
2017-04-04 22:10:46 +0900

    blog.models status 필드 추가 admin 에서 choice 통해서 상태 변경 가능하도록 추가

--- blog/admin.py
+++ blog/admin.py
@@ -4,12 +4,22 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'content_size', 'created_at', 'updated_at']
+    list_display = ['id', 'title', 'content_size', 'status', 'created_at', 'updated_at']
+
+    actions = ['make_published', 'make_draft']
 
     def content_size(self, post):
         return '{}글자'.format(len(post.content))
     content_size.short_description = '글자수'
     #content_size.allow_tags = True # deprecated
+
+    def make_draft(self, request, queryset):
+        updated_count = queryset.update(status='d')
+        self.message_user(request, '{}건의 포스팅을 Draft 상태로 변경'.format(updated_count))
+
+    def make_published(self, request, queryset):
+        updated_count = queryset.update(status='p')
+        self.message_user(request, '{}건의 포스팅을 Published 상태로 변경'.format(updated_count))
 
 # Register your models here.
 # admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -8,11 +8,18 @@ def lnglat_validator(value):
 
 
 class Post(models.Model):
+    STATUS_CHOICES=(
+        ('d', 'Draft'),
+        ('p', 'Publish'),
+        ('w', 'Withdrawn'),
+    )
+
     title = models.CharField(max_length=100)
     content = models.TextField()
     tags = models.CharField(max_length=100, blank=True)
     lnglat = models.CharField(max_length=50, blank = True,
                               validators=[lnglat_validator],
                               help_text='경도,위도 포맷으로 입력')
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
