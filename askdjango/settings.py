8a2cb99af5a3acf148ebd2c7714b62262adb6ca2
Bligh Park <sy131.park@gmail.com>
2017-04-04 21:51:34 +0900

    blog.models.Post ModelAdmin 커스텀

--- askdjango/settings.py
+++ askdjango/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'dojo.apps.DojoConfig',
     'blog',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Post
 
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'created_at', 'updated_at']
+
 # Register your models here.
-admin.site.register(Post)
+# admin.site.register(Post, PostAdmin)
