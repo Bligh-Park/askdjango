943e67b71789e7438e1d161f2b55b4c23d30bbfe
Bligh Park <sy131.park@gmail.com>
2017-04-04 21:58:02 +0900

    blog.admin.PostAdmin 에  list_display, content_size 사용

--- blog/admin.py
+++ blog/admin.py
@@ -4,7 +4,12 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'created_at', 'updated_at']
+    list_display = ['id', 'title', 'content_size', 'created_at', 'updated_at']
+
+    def content_size(self, post):
+        return '{}글자'.format(len(post.content))
+    content_size.short_description = '글자수'
+    #content_size.allow_tags = True # deprecated
 
 # Register your models here.
 # admin.site.register(Post, PostAdmin)
